dc61540789c3c089dfd0941f75b227a0381676d3
Lily Serry <lserry40@stuy.edu>
2024-01-04 14:34:55 -0500

    adding file for handling user list

--- sigTemp.c
+++ sigTemp.c
@@ -10,11 +10,13 @@ void err(int i, char*message){
 static void sighandler( int signo ){
     
     if (signo == SIGINT){
-        printf("Process with PID: %d Exiting due to SIGINT\n", getpid());
+        printf("Process with PID: %d Exiting due to SIGINT\n", getpid());//change to smth involving usr?
         char buffer[50];
         sprintf(buffer, "Process with PID: %d Exiting due to SIGINT\n", getpid());
+       
         int w_file = open("output.txt", O_WRONLY | O_CREAT | O_APPEND, 0600);
-        if(w_file == -1) err();
+        err(w_file, "error opening file");
+        
         write(w_file, &buffer, sizeof(buffer));
         exit(0);
     }
