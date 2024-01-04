4942760a8a894e2af161a3a7dda76f8a7332e76b
Lily Serry <lserry40@stuy.edu>
2024-01-04 14:23:29 -0500

    added err

--- sigTemp.c
+++ sigTemp.c
@@ -1,5 +1,11 @@
 #include <signal.h>
 
+void err(int i, char*message){
+  if(i < 0){
+      printf("Error: %s - %s\n",message, strerror(errno));
+      exit(1);
+  }
+}
 
 static void sighandler( int signo ){
     
