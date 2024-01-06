143e57bca375ec3fdb6dd7e68db2cfbfe61386ab
Lily Serry <lserry40@stuy.edu>
2024-01-05 20:48:09 -0500

    when client connects, it can set its username

--- client.c
+++ client.c
@@ -1,35 +1,55 @@
 #include "networking.h"
 
 
-void clientLogic(int server_socket){
+void clientLogic(int server_socket, char username[50]){
     char buffer[BUFFER_SIZE];
-    char username [50];
+    //char username [50];
     
     //prompt user input
-    
+    while (1) {
+        printf("[%s]: ", buffer);
+        fgets(buffer, sizeof(buffer), stdin);
+        
+        int sbytes = write(server_socket, buffer, strlen(buffer));
+        if (sbytes < 0) {
+            perror("send error");
+            exit(1);
+        }
+        
+        int rbytes = read(server_socket, buffer, sizeof(buffer));
+        if (rbytes > 0) {
+            buffer[rbytes] = '\0';
+            printf("%s", buffer);
+        } else {
+            perror("recv error");
+        }
+    }
+}
     
     //printf("Hello. Please type a message to get started!\n ", username);
     //fgets(chat, sizeof(chat), stdin);
-    
-    
-    // Receive prompt for username change
-    int rbytes = read(server_socket, buffer, sizeof(buffer));
-    if (rbytes > 0) {
-        buffer[rbytes] = '\0';
-        printf("%s", buffer);
-
-        // Input new username
-        fgets(username, sizeof(username), stdin);
-        username[strcspn(username, "\n")] = 0; // Remove newline character
-        write(server_socket, username, strlen(username));
-
-        printf("Username changed to: %s\n", username);
-    } else {
-        perror("recv error");
+    /*
+    while(1){
+        // Receive prompt for username change
+        int rbytes = read(server_socket, buffer, sizeof(buffer));
+        if (rbytes > 0) {
+            buffer[rbytes] = '\0';
+            printf("%s", buffer);
+            
+            // Input new username
+            fgets(username, sizeof(username), stdin);
+            username[strcspn(username, "\n")] = 0; // Remove newline character
+            write(server_socket, username, strlen(username));
+            
+            printf("Username changed to: %s\n", username);
+        } else {
+            perror("recv error");
+        }
+        
+        close(server_socket);
     }
-
-    close(server_socket);
 }
+     */
 
 int main(int argc, char *argv[] ) {
     
@@ -40,11 +60,16 @@ int main(int argc, char *argv[] ) {
     int server_socket = client_tcp_handshake(IP);
     printf("client connected.\n");
     
-    /*
+    
     char name[100];
-    char buffer[BUFFER_SIZE];
     printf("Enter your username: ");
     fgets(name, sizeof(name), stdin);
+    int sbytes = write(server_socket, name, strlen(name));
+    
+    if (sbytes < 0) {
+        perror("send error");
+        exit(1);
+    }
     
     // Remove newline from the end of the username
     for (int i = 0; i < strlen(name); i++){
@@ -53,21 +78,20 @@ int main(int argc, char *argv[] ) {
             break; // Stop once newline is removed
         }
     }
-    */
-    while(1){
-        //char name[100];
-        //printf("Enter your username: ");
-        /*fgets(name, sizeof(name), stdin);
-         
-         //removing new line from end of username
-         for (int i = 0; i < strlen(name); i++){
-         if(name[i]== '\n'|| name[i]== '\r'){
-         name[i] = 0;
-         }
-         }
-         */
-        clientLogic(server_socket);
-        //usleep(50);
-    }
+    
+    //while(1){
+    //char name[100];
+    //printf("Enter your username: ");
+    /*fgets(name, sizeof(name), stdin);
+     
+     //removing new line from end of username
+     for (int i = 0; i < strlen(name); i++){
+     if(name[i]== '\n'|| name[i]== '\r'){
+     name[i] = 0;
+     }
+     }
+     */
+    clientLogic(server_socket, name);
+    //usleep(50);
     close(server_socket);
 }
--- server.c
+++ server.c
@@ -14,29 +14,27 @@ int userCount = 0;
 void subserver_logic(int client_socket, char* username){
     
     char buffer[BUFFER_SIZE];
-    int rbytes = read(client_socket, buffer, sizeof(buffer));
-    
+    printf("[%s] joined the chat\n", username);
     fd_set read_fds;
-
+    
     while (1) {
         FD_ZERO(&read_fds);
         FD_SET(STDIN_FILENO, &read_fds);
         FD_SET(client_socket, &read_fds);
-
+        
         int activity = select(client_socket + 1, &read_fds, NULL, NULL, NULL);
         if (activity < 0) {
             perror("select error");
             break;
-            }
+        }
         
         if(FD_ISSET(STDIN_FILENO, &read_fds)){
             fgets(buffer, sizeof(buffer), stdin); //read from standard in
-            buffer[strlen(buffer)-1] = 0; //remove newline
-            //send message to all clients
-            for (int i = 0; i < userCount + 1; i++){
+            buffer[strlen(buffer) - 1] = '\0';
+            for (int i = 0; i < userCount; i++){
                 if(user_list[i].socket_id != -1){
-                    int Sbytes=write(user_list[i].socket_id, buffer, strlen(buffer));
-                    if(Sbytes==-1){
+                    int Sbytes = write(user_list[i].socket_id, buffer, strlen(buffer));
+                    if(Sbytes == -1){
                         perror("Send error");
                     }
                 }
@@ -51,24 +49,7 @@ void subserver_logic(int client_socket, char* username){
                 printf("[%s] disconnected\n", username);
                 break;
             }
-            else{
-                buffer[Rbytes] = '\0';
-                if (strncmp(buffer, "/username ", 10) == 0) {
-                    // Extract the new username
-                    char new_username[50];
-                    sscanf(buffer + 10, "%s", new_username);
-                    
-                    // Find the user and update the username
-                    for (int i = 0; i < userCount; ++i) {
-                        if (user_list[i].socket_id == client_socket) {
-                            strcpy(user_list[i].username, new_username);
-                            printf("[%s] changed username to [%s]\n", username, new_username);
-                            break;
-                        }
-                    }
-                }
-            }
-
+            
             // Broadcast the received message to all clients
             for (int i = 0; i < userCount + 1; i++) {
                 if (user_list[i].socket_id != -1 && user_list[i].socket_id != client_socket) {
@@ -80,7 +61,7 @@ void subserver_logic(int client_socket, char* username){
             }
         }
     }
-
+    
     close(client_socket);
     //printf("server waiting for another request\n");
 }
