5febb4d5eb4fae7bb648b710f9db4c0d0754746c
margaux scandura <mscandura40@stuy.edu>
2024-01-09 17:06:15 -0500

    forgot to add my history.h

---
+++ history.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+#ifndef HISTORY_H
+#define HISTORY_H
+#define BUFFER_SIZE 1024
+void reader(char *username, char *message);
+void creator();
+
+#endif
+
+
