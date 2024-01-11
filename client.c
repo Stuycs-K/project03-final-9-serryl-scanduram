4ee285593b15547ed156c929b4c6935e593c66a3
margaux scandura <mscandura40@stuy.edu>
2024-01-11 16:54:17 -0500

    final commits

--- client.c
+++ client.c
@@ -85,6 +85,7 @@ void clientLogic(int server_socket, char username[50]){
                 if (save[0]=='y'){
                     char message[BUFFER_SIZE + 55];
                     snprintf(message, sizeof(message), "[%s]: %s", username, buffer);
+                    printf("%s\n", message);
                     writer(FILENAME, buffer);
                 }
                 int sbytes = write(server_socket, buffer, strlen(buffer));
